c7b40f9b5917c7ad38eb3457e7fcda3473764d25
TUadmin2026 <translationsuniverse@gmail.com>
2026-07-27 09:36:25 +0200

    Update gengo_hunter.py

--- gengo_hunter.py
+++ gengo_hunter.py
@@ -1,8 +1,100 @@
-print("CIAO GITHUB ACTIONS")
-
 import os
+import json
+import hashlib
+import requests
+import feedparser
 
-print("Token presente:", bool(os.environ.get("TELEGRAM_TOKEN")))
-print("Chat presente:", bool(os.environ.get("CHAT_ID")))
 
-print("FINE TEST")
+RSS_URL = "https://gengo.com/rss/available_jobs/7f2172912f19d67534e9668a57900dec5545d95a20652351863981/"
+
+
+TELEGRAM_TOKEN = os.environ["TELEGRAM_TOKEN"]
+CHAT_ID = os.environ["CHAT_ID"]
+
+
+MEMORY_FILE = "seen_jobs.json"
+
+
+print("🚀 Gengo Hunter avviato")
+
+
+def send_telegram(message):
+
+    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
+
+    data = {
+        "chat_id": CHAT_ID,
+        "text": message,
+        "disable_web_page_preview": False
+    }
+
+    response = requests.post(url, json=data)
+
+    print("Telegram status:", response.status_code)
+
+
+
+def load_seen():
+
+    try:
+        with open(MEMORY_FILE, "r") as f:
+            return json.load(f)
+
+    except:
+        return []
+
+
+
+def save_seen(data):
+
+    with open(MEMORY_FILE, "w") as f:
+        json.dump(data, f)
+
+
+
+def main():
+
+    seen = load_seen()
+
+    feed = feedparser.parse(RSS_URL)
+
+    print("Feed status:", getattr(feed, "status", "n/a"))
+    print("Jobs trovati:", len(feed.entries))
+
+
+    for entry in feed.entries:
+
+        job_id = hashlib.md5(
+            entry.link.encode()
+        ).hexdigest()
+
+
+        if job_id in seen:
+            continue
+
+
+        message = f"""
+🚨 GENGO JOB DISPONIBILE
+
+🌍 EN → IT
+
+📌 {entry.title}
+
+📝 {entry.description}
+
+🔗 Apri lavoro:
+{entry.link}
+"""
+
+
+        send_telegram(message)
+
+        seen.append(job_id)
+
+
+    save_seen(seen)
+
+
+
+if __name__ == "__main__":
+    main()
